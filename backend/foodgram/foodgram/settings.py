18d58c919813a46bc9942e14b5f7b60fffb96a11
Ivan <ivanartamonov@MacBook-Air-Ivan.local>
2023-06-11 15:42:27 +0300

    upd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,11 +7,17 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-ew@$8vmre6hu1r5@)-rj!3d8&g47q)*(zk#n%m&)mqhuwvxfh9'
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'backend',
+    'localhost',
+    '84.252.139.111',
+    'db'
+]
 
 
 INSTALLED_APPS = [
